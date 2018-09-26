8d8247f144f01b039f126380a6f500d5e4b918c8
Shohei Morinaga <Shohei Morinaga>
2018-09-27 00:46:10 +0900

    fixed minor bug

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 apt-get update
 apt-get install -y  cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-pip install -r requirements.txt
