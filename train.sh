18f6a608627896f4ae3be86c0daa9688d9f47ed1
Shohei Morinaga <Shohei Morinaga>
2018-09-28 12:24:47 +0900

    changed train.sh to select train and valid data seperately

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python crnn_main.py --adadelta --niter 100 --displayInterval 100 --valInterval 100 --trainroot ../lmdb/train/ --valroot ../lmdb/train/ --cuda --random_sample --workers 1 --keep_ratio
+python crnn_main.py --adadelta --niter 100 --displayInterval 500 --valInterval 100 --trainroot ../lmdb/train/ --valroot ../lmdb/valid/ --cuda --random_sample --workers 1 --keep_ratio
